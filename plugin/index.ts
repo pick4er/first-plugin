c26ae24f004a84c083c01b79bd7cd46a03b2682c
pick4er <pick4er@gmail.com>
2019-08-18 14:42:53 +0300

    Remove last compiled files

--- plugin/index.ts
+++ plugin/index.ts
@@ -1,27 +1,68 @@
-import { Compiler } from 'webpack';
+import path from 'path';
+import { sync as delSync } from 'del';
+import { Compiler, Stats, compilation as compilationType } from 'webpack';
+
+type Compilation = compilationType.Compilation;
 
 class FirstPlugin {
+  private outputPath: string;
+
+  constructor() {
+    this.outputPath = '';
+
+    this.apply = this.apply.bind(this);
+    this.handleEmit = this.handleEmit.bind(this);
+    this.removeFiles = this.removeFiles.bind(this);
+  }
+
   apply(compiler: Compiler) {
+    if (!compiler.options.output || !compiler.options.output.path) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        'first-plugin: options.output.path not defined. Plugin disabled...',
+      );
+
+      return;
+    }
+
+    this.outputPath = compiler.options.output.path;
+
     const hooks = compiler.hooks;
-
-    hooks.emit.tapAsync('FirstPlugin', (compilation, cbk) => {
-      let filelist = 'In this build:\n\n';
-
-      for (let filename in compilation.assets) {
-        filelist += '- ' + filename + '\n';
-      }
-
-      compilation.assets['filelist.md'] = {
-        source() {
-          return filelist;
-        },
-        size() {
-          return filelist.length;
-        },
-      };
-
-      cbk();
+    hooks.emit.tap('first-plugin', compilation => {
+      this.handleEmit(compilation);
     });
+  }
+
+  handleEmit(compilation: Compilation) {
+    const stats = compilation.getStats();
+    if (stats.hasErrors()) {
+      console.log(
+        'first-plugin: pausing due to webpack errors',
+      );
+
+      return;
+    }
+
+    this.removeFiles();
+  }
+
+  removeFiles() {
+    try {
+      const deleted = delSync(['**/*'], {
+        cwd: this.outputPath,
+        dot: true
+      });
+
+      deleted.forEach(file => {
+        const filename = path.relative(process.cwd(), file);
+
+        console.warn(
+          `first-plugin: ${filename} removed`,
+        );
+      });
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
